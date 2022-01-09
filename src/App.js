1e87ac56c497e026fe16e3f43fdd3891fe6fd7bc
fachrul-rh <fachrulstroid@gmail.com>
2022-01-09 14:32:44 +0700

    add components navbar&container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+
 function App() {
+  const [emojisData, setEmojisData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      setLoading(true);
+      try {
+        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+
+        setEmojisData(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+
+        setError(true);
+        setLoading(false);
+      }
+    }
+
+    fetchEmojis();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
   );
 }
 
